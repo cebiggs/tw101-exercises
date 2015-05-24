27da8c01fa005a1067c7d75a985fad3bb1037774
Cannon Biggs <cebiggs@unomaha.edu>
2015-05-24 16:20:49 -0500

    Add exercise 2

--- src/main/java/com/thoughtworks/tw101/exercises/exercise2/Accumulator.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise2/Accumulator.java
@@ -1,4 +1,14 @@
 package com.thoughtworks.tw101.exercises.exercise2;
 
 public class Accumulator {
+
+    public static int numOfCalls = 0;
+
+    public void increment() {
+        numOfCalls++;
+    }
+
+    public void total() {
+        System.out.print(numOfCalls);
+    }
 }
