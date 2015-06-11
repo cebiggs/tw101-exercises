2c78bdb570a95bf793ce8e3d0bad9d836581a9c0
Cannon Biggs <cebiggs@unomaha.edu>
2015-06-10 21:50:23 -0500

    Create classes for OrcTests and TrollTests and implement shouldReturnCurrentHitpoints for OrcTests

---
+++ src/test/java/exercise6/OrcTests.java
@@ -0,0 +1,35 @@
+package exercise6;
+
+import com.thoughtworks.tw101.exercises.exercise6.Orc;
+import org.junit.Before;
+import org.junit.Test;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Created by cannon on 6/10/15.
+ */
+public class OrcTests {
+
+    private Orc orc;
+
+    @Before
+    public void setUp() {
+        orc = new Orc();
+    }
+
+    @Test
+    public void shouldTakeDamage() {
+
+    }
+
+    @Test
+    public void shouldSetName() {
+
+    }
+
+    @Test
+    public void shouldReturnCurrentHitpoints() {
+        assertThat(orc.currentHitpoints(), is(20));
+    }
+}
---
+++ src/test/java/exercise6/TrollTests.java
@@ -0,0 +1,24 @@
+package exercise6;
+
+import org.junit.Test;
+
+/**
+ * Created by cannon on 6/10/15.
+ */
+public class TrollTests {
+
+    @Test
+    public void shouldTakeDamage() {
+
+    }
+
+    @Test
+    public void shouldSetName() {
+
+    }
+
+    @Test
+    public void shouldReturnCurrentHitpoints() {
+
+    }
+}
