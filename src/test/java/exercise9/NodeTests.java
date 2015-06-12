e3b5778646de9d140ea28160f2b7fcee8f4190d9
Cannon Biggs <cebiggs@unomaha.edu>
2015-06-11 23:16:58 -0500

    Implement NodeTests

---
+++ src/test/java/exercise9/NodeTests.java
@@ -0,0 +1,27 @@
+package exercise9;
+
+import com.thoughtworks.tw101.exercises.exercise9.Node;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+/**
+ * Created by cannon on 6/11/15.
+ */
+public class NodeTests {
+
+    @Test
+    public void shouldReturnNamesOfAllNodesInAlphabeticalOrder() {
+        Node root = new Node("Cecil");
+
+        root.add("Michelle");
+        root.add("Bill");
+
+        assertThat(root.names().get(0), is("Bill"));
+        assertThat(root.names().get(1), is("Cecil"));
+        assertThat(root.names().get(2), is("Michelle"));
+    }
+}
