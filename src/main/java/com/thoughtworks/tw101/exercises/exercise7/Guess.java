0c7ef32f24a29f03018c298232e04a41b066131a
Cannon Biggs <cebiggs@unomaha.edu>
2015-06-11 22:36:49 -0500

    Update imports

--- src/main/java/com/thoughtworks/tw101/exercises/exercise7/Guess.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise7/Guess.java
@@ -2,7 +2,6 @@ package com.thoughtworks.tw101.exercises.exercise7;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.PrintStream;
 
 /**
